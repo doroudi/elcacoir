c83edadee66d5304b59a9046082a590c7ae46bed
Saeid Doroudi <doroudi@outlook.com>
2021-11-12 20:20:17 +0330

    add google maps

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import SectionBar from './components/section-bar.vue'
 import "boxicons"
 
 Vue.config.productionTip = false
+
 Vue.component('section-bar', SectionBar);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
