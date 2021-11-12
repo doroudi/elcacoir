a7569f8873c7500d3a1f3bb27f9b9bd9bbf5123e
Saeid Doroudi <doroudi@outlook.com>
2021-11-12 12:23:30 +0330

    implement basics and complete serivces

---
+++ src/components/footer.vue
@@ -0,0 +1,67 @@
+<template>
+  <footer>
+    <div class="copywrite">
+      <span>تمامی حقوق برای شرکت فنی و مهندسی الکا محفوظ است - 1400</span>
+    </div>
+    <nav class="social">
+      <ul>
+        <li>
+          <a title="فیسبوک" href="https://www.facebook.com/ElcaResana/">
+            <box-icon type="logo" name="facebook"></box-icon>
+          </a>
+        </li>
+        <li>
+          <a title="توییتر" href="https://twitter.com/ElcaResana">
+            <box-icon type="logo" name="twitter"></box-icon>
+          </a>
+        </li>
+        <li>
+          <a
+            title="اینستاگرام"
+            href="https://www.instagram.com/elca_ulrtrasonic/"
+          >
+            <box-icon type="logo" name="instagram"></box-icon>
+          </a>
+        </li>
+        <li>
+          <a
+            title="لینکدین"
+            href="https://www.linkedin.com/elcaco/"
+          >
+            <box-icon type="logo" name="linkedin"></box-icon>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="go-top">
+      <div class="button">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 100 25"
+          enable-background="new 0 0 100 25"
+          xml:space="preserve"
+        >
+          <path
+            id="bottom-curve"
+            fill="#B4E3F2"
+            d="M100, 25 C62.5, 25, 62.5, 0,  50, 0  S37.5, 25, 0,  25"
+          ></path>
+        </svg>
+        <a href="#" id="go-home" style="display: block"
+          >
+            <box-icon name="chevron-up"></box-icon>
+          </a>
+      </div>
+      <div class="bottom-bar"></div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
